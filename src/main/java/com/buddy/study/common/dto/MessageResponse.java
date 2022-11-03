c1a4ca4b585fe1e37d80363c7272af8a2f815c77
myeongju-kim <mjoo1106@naver.com>
2022-11-03 14:14:36 +0900

    feat: create message response

---
+++ src/main/java/com/buddy/study/common/dto/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.buddy.study.common.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
