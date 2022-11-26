582b1b83c2279a9de2f0f93c6b71a5dff472cda2
myeongju-kim <mjoo1106@naver.com>
2022-11-26 15:28:14 +0900

    feat: create group detail functions

--- src/main/java/com/buddy/study/group/service/GroupService.java
+++ src/main/java/com/buddy/study/group/service/GroupService.java
@@ -6,13 +6,17 @@ import com.buddy.study.common.dto.ErrorCode;
 import com.buddy.study.common.dto.MessageResponse;
 import com.buddy.study.common.exception.ConflictException;
 import com.buddy.study.group.domain.Mapping;
-import com.buddy.study.group.domain.StudyGroup;
+import com.buddy.study.group.domain.Group;
 import com.buddy.study.group.dto.CreateResponse;
+import com.buddy.study.group.dto.DetailResponse;
+import com.buddy.study.group.dto.DetailsResponse;
 import com.buddy.study.group.dto.GroupRequest;
 import com.buddy.study.group.dto.GroupResponse;
 import com.buddy.study.group.dto.GroupType;
 import com.buddy.study.group.repository.GroupRepository;
 import com.buddy.study.group.repository.MappingRepository;
+import com.buddy.study.time.dto.TimeResponse;
+import com.buddy.study.time.service.TimeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,15 +30,16 @@ public class GroupService {
     private final GroupRepository groupRepository;
     private final MappingRepository mappingRepository;
     private final AccountService accountService;
+    private final TimeService timeService;
     private MessageResponse messageResponse=new MessageResponse();
-    public StudyGroup findGroup(Long gid){
+    public Group findGroup(Long gid){
         return groupRepository.findById(gid).orElse(null);
     }
     public CreateResponse createGroup(UUID uid, GroupRequest groupRequest){
         Account account= accountService.findUser(uid);
 
         // 그룹 생성
-        StudyGroup group=new StudyGroup();
+        Group group=new Group();
         group.setName(groupRequest.getName());
         group.setTime(groupRequest.getTime());
         group.setMaxPerson(groupRequest.getMaxPerson());
@@ -50,11 +55,11 @@ public class GroupService {
         return new CreateResponse(group.getId());
     }
     public MessageResponse joinGroup(UUID uid, Long gid){
-        StudyGroup group=findGroup(gid);
+        Group group=findGroup(gid);
         Account account= accountService.findUser(uid);
-        List<StudyGroup> studyGroups=mappingRepository.findAllByAccount(account);
+        List<Group> groups =mappingRepository.findAllByAccount(account);
         //이미 가입되어 있는 그룹인 경우 에러코드
-        if(studyGroups.contains(group))
+        if(groups.contains(group))
             throw new ConflictException(ErrorCode.DUPLICATE_GROUP);
         //최대 인원이라 가입을 하지 못하는 경우 에러코드
         if(group.getCurPerson()>=group.getMaxPerson())
@@ -71,8 +76,8 @@ public class GroupService {
     }
     public List<GroupResponse> loadGroup(){
         List<GroupResponse> groupsResponse=new ArrayList<>();
-        List<StudyGroup> groups=groupRepository.findAllByOrderByIdDesc();
-        for(StudyGroup group:groups){
+        List<Group> groups=groupRepository.findAllByOrderByIdDesc();
+        for(Group group:groups){
             GroupResponse groupResponse=new GroupResponse();
             groupResponse.setCurPerson(group.getCurPerson());
             groupResponse.setMaxPerson(group.getMaxPerson());
@@ -82,5 +87,26 @@ public class GroupService {
         }
         return groupsResponse;
     }
+    public DetailsResponse detailGroup(Long gid) {
+        DetailsResponse detail = new DetailsResponse();
+        List<DetailResponse> detailResponses = new ArrayList<>();
+        Group group = groupRepository.findById(gid).orElse(null);
+        detail.setTarTime(group.getTime());
 
+        List<Mapping> mappings = mappingRepository.findAllByGroup(group);
+        for (Mapping mapping : mappings) {
+            DetailResponse detailResponse = new DetailResponse();
+            detailResponse.setName(mapping.getAccount().getName());
+            TimeResponse time = timeService.getTodayTime(mapping.getAccount().getId());
+            detailResponse.setCurTime(time.getTime());
+            if (time.getTime() < group.getTime()) {
+                detailResponse.setCompletion(false);
+            } else {
+                detailResponse.setCompletion(true);
+            }
+            detailResponses.add(detailResponse);
+        }
+        detail.setDetailResponses(detailResponses);
+        return detail;
+    }
 }
