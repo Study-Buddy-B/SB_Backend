e1c07bb5c2ccb6ac01c8a11830c1dac3c78eaeac
myeongju-kim <mjoo1106@naver.com>
2022-11-26 14:15:53 +0900

    feat: create home user response type

---
+++ src/main/java/com/buddy/study/user/dto/UserResponse.java
@@ -0,0 +1,14 @@
+package com.buddy.study.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserResponse {
+  private Float curTime;
+  private Integer tarTime;
+  private Float temperature;
+}
