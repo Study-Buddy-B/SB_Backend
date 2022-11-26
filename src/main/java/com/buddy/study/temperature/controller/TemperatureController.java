ab593ee754835710de0ab6fe05d7008575b54532
myeongju-kim <mjoo1106@naver.com>
2022-11-26 15:27:00 +0900

    feat: apply swagger to temperature controller

--- src/main/java/com/buddy/study/temperature/controller/TemperatureController.java
+++ src/main/java/com/buddy/study/temperature/controller/TemperatureController.java
@@ -3,6 +3,7 @@ package com.buddy.study.temperature.controller;
 import com.buddy.study.temperature.dto.CreateRequest;
 import com.buddy.study.temperature.dto.TemperatureResponse;
 import com.buddy.study.temperature.service.TemperatureService;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/temperature")
 public class TemperatureController {
     private final TemperatureService noticeService;
-
+    @Operation(summary = "온도 업데이트")
     @PostMapping
     public ResponseEntity<TemperatureResponse> createTemperature(@RequestHeader("Authorization") UUID userId, @RequestBody CreateRequest request) {
         return ResponseEntity.ok(noticeService.createTemperature(userId, request));
     }
-
+    @Operation(summary = "온도 조회")
     @GetMapping
     public ResponseEntity<TemperatureResponse> getTemperature(@RequestHeader("Authorization") UUID userId) {
         return ResponseEntity.ok(noticeService.getTemperature(userId));
