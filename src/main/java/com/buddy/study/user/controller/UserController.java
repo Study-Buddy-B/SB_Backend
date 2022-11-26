7e4493b60a216b4cc55ed63a412e03cfa7f01270
myeongju-kim <mjoo1106@naver.com>
2022-11-26 15:26:31 +0900

    feat: apply swagger to user controller

--- src/main/java/com/buddy/study/user/controller/UserController.java
+++ src/main/java/com/buddy/study/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.buddy.study.common.dto.MessageResponse;
 import com.buddy.study.user.dto.TimeRequest;
 import com.buddy.study.user.dto.UserResponse;
 import com.buddy.study.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
+    @Operation(summary = "홈 화면")
     @GetMapping("")
     public ResponseEntity<UserResponse> loadUser(@RequestHeader("Authorization") UUID userId){
         return ResponseEntity.status(HttpStatus.OK)
             .body(userService.loadUser(userId));
     }
+    @Operation(summary = "목표 시간 설정")
     @PostMapping("/time")
     public ResponseEntity<MessageResponse> timeUser(@RequestHeader("Authorization") UUID userId, @RequestBody
         TimeRequest timeRequest){
