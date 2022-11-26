210c30fbc67ae7bd46817bae60a7c55af203b81e
myeongju-kim <mjoo1106@naver.com>
2022-11-26 14:17:21 +0900

    fix: modify param annotations

--- src/main/java/com/buddy/study/time/repository/TimeRepository.java
+++ src/main/java/com/buddy/study/time/repository/TimeRepository.java
@@ -7,14 +7,15 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TimeRepository extends JpaRepository<Time, Long> {
     @Query("select t from Time t where t.account = :account and t.date like :date% order by t.date")
-    List<Time> findAllByDateAndAccount(String date, Account account); // date YYYY-MM
+    List<Time> findAllByDateAndAccount(@Param("date") String date, @Param("account") Account account); // date YYYY-MM
 
     @Query("select new com.buddy.study.time.dto.TimeReportResponse(AVG(t.time), SUM(t.time), MAX(t.time), MIN(t.time)) from Time t where t.account = :account and t.date like :date%")
-    TimeReportResponse findReportByDateAndAccount(String date, Account account);
+    TimeReportResponse findReportByDateAndAccount(@Param("date") String date, @Param("account") Account account);
 
     @Query("select t from Time t where t.account = :account and t.date like :date%")
-    Optional<Time> findOneByDateAndAccount(String date, Account account);
+    Optional<Time> findOneByDateAndAccount(@Param("date") String date, @Param("account") Account account);
 }
